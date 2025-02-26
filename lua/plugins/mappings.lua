66a364cfffdb205c58b02aef9b09d4cf0a351322
Andrew Elster <andrew@andrewelster.com>
2025-02-26 15:37:48 -0500

    Adds example mappings
    - Uses bn and bb for buffer navigation like with Lunarvim

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,7 +5,9 @@ return {
     opts = {
       mappings = {
         n = {
-         ["<Leader>a"] = { "<cmd>echo 'Hello World!'<cr>", desc = "Say Hello world" },
+          -- ["<Leader>a"] = { "<cmd>echo 'Hello World!'<cr>", desc = "Say Hello world" },
+          ["<Leader>bn"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+          ["<Leader>bb"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
         }
       }
     }
