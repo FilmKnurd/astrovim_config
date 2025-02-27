93ccd4450f59cf4f24f772f4ee3834c291ea3a90
Andrew Elster <andrew@andrewelster.com>
2025-02-27 13:07:04 -0500

    Adds defaults to neo-tree

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -82,4 +82,16 @@ return {
       )
     end,
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+     filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_hidden = false,
+        },
+      },
+    },
+  }
 }
